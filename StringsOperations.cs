b79f4a92a61403226d95f2e0eb68cc4026d21059
Ashutosh Rai <raiashutech@gmail.com>
2019-10-12 16:21:29 -0500

    Update StringsOperations.cs

--- StringsOperations.cs
+++ StringsOperations.cs
@@ -1,5 +1,18 @@
+            //Removes the beginning characters from 0 to defined index, and them trims the spaces from both side           
             public string GetTextAfterRemovingASubString()
             {
                  StringBuilder sb = new StringBuilder(textString);
                  return sb.Remove(0, indexOfAnCharacter).ToString().Trim();
             }
+
+            // Retrive the fields of a class and retrive the assigned values there
+            public void AccessTheFieldValuesOfAClass()
+            {
+            Type type = typeof(ClassABC);
+             //To Retrive the properties of a class we can user PropertyInfo and GetProperty()
+            foreach (FieldInfo field in type.GetFields())
+            {
+               //To Get The Name Of field and not the assigned value there we can user field.Name
+               ApplicationHelper.VerifyLabelExit(testControl, field.GetValue(null).ToString());
+            }
+        }
